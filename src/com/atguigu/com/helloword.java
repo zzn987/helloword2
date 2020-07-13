70256e24eefec7a61d14fbe913ab937121b09f07
dingyang <zzn987@126.com>
2020-07-13 17:00:49 +0800

    first commit

---
+++ src/com/atguigu/com/helloword.java
@@ -0,0 +1,14 @@
+package com.atguigu.com;
+
+public class helloword {
+
+    public static void main(String[] args){
+        System.out.println("helloword!!");
+        System.out.println("helloword!!");
+        System.out.println("helloword!!");
+    }
+
+}
+
+
+
