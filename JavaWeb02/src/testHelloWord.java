166c29464ee2333bcde74c3afd8c4720ec5ba68e
dingyang <zzn987@126.com>
2020-07-18 09:18:00 +0800

    test2

---
+++ JavaWeb02/src/testHelloWord.java
@@ -0,0 +1,9 @@
+public class testHelloWord {
+    public static void main(String[] args){
+        System.out.println("helloword!!");
+        System.out.println("helloword!!");
+        System.out.println("helloword!!");
+    }
+}
+
+
--- src/com/atguigu/com/test.java
+++ src/com/atguigu/com/test.java
@@ -6,7 +6,8 @@ public class test {
         System.out.println("test!");
         System.out.println("test2!");
         System.out.println("test2!");
-        System.out.println("test33121!");
+        System.out.println("test331212!");
 
     }
 }
+
